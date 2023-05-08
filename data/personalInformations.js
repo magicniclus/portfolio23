4a1523e21404b90e08688c9b7bf634f2e907210d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-08 09:58:06 +0200

    Create personalInformations.js

---
+++ data/personalInformations.js
@@ -0,0 +1,22 @@
+export const personalInformations = {
+  client: [
+    "Terabois",
+    "Maprimerenov-info",
+    "Adsventure",
+    "Avenue-immo",
+    "Portail-habitat",
+  ],
+  technologies: [
+    "React Js",
+    "Next Js",
+    "Javascript",
+    "Sass",
+    "Tailwind",
+    "Gsap",
+    "Firebase",
+    "Google Api",
+    "Mapbox",
+  ],
+  learning: ["Three Js", "Shopify"],
+  certification: ["Google ads search"],
+};
