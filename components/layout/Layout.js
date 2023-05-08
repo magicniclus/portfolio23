970b373f881a14f464f2c4ca5c05c92fa7943fd3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-08 09:57:49 +0200

    Create Layout.js

---
+++ components/layout/Layout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Head from "next/head";
+
+const Layout = (props) => {
+  const robots = props.robots || "follow";
+  const canonical = props.canonical || "";
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <meta name="description" content={props.title} />
+        <meta name="robots" content={"index, " + robots} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="language" content="fr" />
+        {/* <link
+          rel="canonical"
+          href={
+            "https://www.adsventure-agency.com/agences/" +
+            props.title.toLowerCase()
+          }
+        /> */}
+      </Head>
+      <div className="h-screen bg-dark w-screen flex flex-col justify-between mx-auto max-w-[2000px]">
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
