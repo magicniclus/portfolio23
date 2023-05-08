553c7b8087d39fcbbc5080d5ebb2d561e3e66528
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-08 09:58:15 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: "GTM-K3X5GGQ",
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return (
+    <>
+      <Head>
+        {/* <link rel="shortcut icon" href="/images/AdsVenture-favicon.png" /> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
