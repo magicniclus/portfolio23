5bcc5645f2b45d31fe645851329ca02ee21841fe
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-08 09:57:54 +0200

    Create TextContainer.js

---
+++ components/molecules/TextContainer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TextContainer = () => {
+  return (
+    <div>
+      <p className="text-clear text-xs">
+        Jeune développeur passionné, ambitieux et très curieux, je suis
+        particulièrement attiré par l'écosystème JavaScript. Toujours à l'affût
+        des nouvelles technologies, je porte un intérêt tout particulier pour
+        React et Next.js, qui offre de nombreux avantages en matière de
+        performance, de référencement et d'expérience utilisateur.
+      </p>
+      <p className="mt-6 text-clear text-xs">
+        En complément de mes compétences en Front-end, j'ai acquis une expertise
+        dans l'utilisation de Firebase et Google API, qui permettent une gestion
+        efficace des bases de données, de l'authentification et des services
+        cloud. Je m'attache également à accorder une importance capitale aux
+        détails, en utilisant des outils tels que GSAP pour les animations et
+        Sass pour un code optimisé et facilement compréhensible.
+      </p>
+      <p className="mt-6 text-clear text-xs">
+        À l'écoute de mes clients, je saurai conseiller, orienter et adapter mon
+        savoir-faire à vos attentes, en mettant à profit mes connaissances
+        approfondies et mon expérience pratique avec ces technologies
+        innovantes.
+      </p>
+    </div>
+  );
+};
+
+export default TextContainer;
