5dcff7c101b451bf745e25ccaf416d96bec10ac8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-09 12:27:05 +0200

    Update reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,9 +1,15 @@
 const initState = {
   loader: false,
+  changePage: false,
 };
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
+    case "UPDATE_PAGE":
+      return {
+        ...state,
+        changePage: action.payload,
+      };
     default:
       return state;
   }
