0c20c721679fec44c41405ba504871f7493f4e2e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-10 14:06:32 +0200

    Update Footer.js

--- components/organismes/footer/Footer.js
+++ components/organismes/footer/Footer.js
@@ -1,24 +1,81 @@
 import Link from "next/link";
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { useSelector, useDispatch } from "react-redux";
 
 const Footer = () => {
+  const gitRef = useRef(null);
+  const linkdinRef = useRef(null);
+  const twitterRef = useRef(null);
+  const mailRef = useRef(null);
+  const titleRef = useRef(null);
+
+  const state = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  const makeClass = `h-6 ${state.firstView ? "translate-y-6" : ""}`;
+
+  useEffect(() => {
+    if (state.firstView) {
+      const elements = [
+        gitRef.current,
+        linkdinRef.current,
+        twitterRef.current,
+        mailRef.current,
+        titleRef.current,
+      ];
+      gsap.fromTo(
+        elements,
+        { y: 30, opacity: 0 },
+        {
+          duration: 0.5,
+          y: 0,
+          opacity: 1,
+          ease: "power5.out",
+          onComplete: () => {
+            dispatch({ type: "UPDATE_FIRST_VIEW", payload: false });
+          },
+        }
+      );
+    } else null;
+  }, []);
+
   return (
     <footer className="w-full py-8 px-16 flex items-end justify-between">
       <div className="flex">
-        <Link className="mr-5" href="/">
-          <img className="h-6" src="./img/reseaux/github.png" />
+        <Link className="mr-5 overflow-hidden" href="/">
+          <img
+            ref={gitRef}
+            className={makeClass}
+            src="./img/reseaux/github.png"
+          />
         </Link>
-        <Link className="mr-5" href="/">
-          <img className="h-6" src="./img/reseaux/linkdin.png" />
+        <Link className="mr-5 overflow-hidden" href="/">
+          <img
+            ref={linkdinRef}
+            className={makeClass}
+            src="./img/reseaux/linkdin.png"
+          />
         </Link>
-        <Link className="mr-5" href="/">
-          <img className="h-6" src="./img/reseaux/twitter.png" />
+        <Link className="mr-5 overflow-hidden" href="/">
+          <img
+            ref={twitterRef}
+            className={makeClass}
+            src="./img/reseaux/twitter.png"
+          />
         </Link>
-        <Link className="" href="/">
-          <img className="h-6" src="./img/reseaux/mail.png" />
+        <Link className=" overflow-hidden" href="/">
+          <img
+            ref={mailRef}
+            className={makeClass}
+            src="./img/reseaux/mail.png"
+          />
         </Link>
       </div>
-      <h2 className="font-light text-xs text-clear">Front-end developer</h2>
+      <h2 ref={titleRef} className="font-light text-xs text-clear">
+        Front-end developer
+      </h2>
     </footer>
   );
 };
