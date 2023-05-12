90b25efb9db3310061e4544f649f91b18a143dbf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-12 07:08:10 +0200

    Update reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -2,6 +2,12 @@ const initState = {
   loader: false,
   changePage: false,
   firstView: true,
+  galerieParams: {
+    clicked: null,
+    urls: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 5, 7, 8, 2, 4, 9, 6].map(
+      (u) => `/${u}.jpg`
+    ),
+  },
 };
 
 const reducer = (state = initState, action) => {
