82b190492726582ae17d54a13acdf8cee1a0a674
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-08 17:47:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Nav from "../components/organismes/navigations/Nav";
 import Layout from "../components/layout/Layout";
 import Footer from "../components/organismes/footer/Footer";
 import Galerie from "../components/organismes/galerie/Galerie";
-import Blob from "../components/molecules/Blob";
 
 export default function Home() {
   const state = useState((state) => state);
@@ -21,7 +20,6 @@ export default function Home() {
         <Nav />
       </header>
       <main>
-        <Blob />
         <Galerie />
       </main>
       <Footer />
