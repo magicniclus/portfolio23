3bb1d304edcd473770e5f39a99fc3d83ebe03bb3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-12 11:09:54 +0200

    Update Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -12,13 +12,6 @@ const Layout = (props) => {
         <meta name="robots" content={"index, " + robots} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="language" content="fr" />
-        {/* <link
-          rel="canonical"
-          href={
-            "https://www.adsventure-agency.com/agences/" +
-            props.title.toLowerCase()
-          }
-        /> */}
       </Head>
       <div className="min-h-screen bg-dark w-screen flex flex-col justify-between item-center mx-auto max-w-[2000px]">
         {props.children}
