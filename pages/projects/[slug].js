f10bf63052d7d0af57d0a823fb1adb5bc2a3e477
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-12 15:39:10 +0200

    Update [slug].js

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,13 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { gsap } from "gsap";
 import { project } from "../../data/projects";
 import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter";
 import ProjectLayout from "../../components/layout/ProjectLayout";
 
 const Projet = ({ post }) => {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
   const titleRef = useRef(null);
 
   useEffect(() => {
@@ -15,38 +12,80 @@ const Projet = ({ post }) => {
   }, []);
 
   return (
-    <ProjectLayout bg={post.color} text={post.textColor}>
+    <ProjectLayout bg={post.color} text={post.textColor} all={post}>
       <main
-        className="w-screen flex justify-center items-center min-h-[calc(100vh-112px-80px)]"
+        className="w-screen flex justify-center items-center h-full min-h-[calc(100vh-112px-80px)] px-16"
         style={{ backgroundColor: post.color }}
       >
-        <div className="flex flex-col">
-          <a href="/" className="flex items-center flex-col">
-            <h1
-              ref={titleRef}
-              className="text-xs text-center font-bold"
-              style={{ color: post.textColor }}
-            >
-              {capitalizeFirstLetter(post.title)}
-            </h1>
-            <div
-              className={`w-4 h-0.5 rotate-90 mt-3`}
-              style={{ backgroundColor: post.textColor }}
-            ></div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill={post.textColor}
-              className="w-4 h-4 mt-3"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-                clipRule="evenodd"
+        <div className="flex flex-col justify-around h-full w-screen">
+          <div className="flex justify-around">
+            <div className="w-5/12 flex justify-between flex-col">
+              <div></div>
+              <div>
+                <a href={post.url} className="cursor-pointor">
+                  <img
+                    className="w-80 max-h-[250px] object-contain"
+                    src={post.logo}
+                    alt="logo"
+                  />
+                </a>
+                <ul className="flex mt-5">
+                  {post.techno.map((item, index) => (
+                    <li
+                      className="text-xs"
+                      style={{ color: post.textColor }}
+                      key={index}
+                    >
+                      {item}
+                      {index !== post.techno.length - 1 ? (
+                        <span
+                          className="mx-2"
+                          style={{ color: post.textColor }}
+                        >
+                          -
+                        </span>
+                      ) : null}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <ul className="text-xs" style={{ color: post.textColor }}>
+                <li className="mt-1">
+                  A <span className="mx-2"></span> STATUS: {post.status}
+                </li>
+                <li className="mt-1">
+                  B <span className="mx-2"></span> COMPLETED: {post.years}
+                </li>
+                <li className="mt-1">
+                  C <span className="mx-2"></span> ROLE: {post.role}
+                </li>
+                <li className="mt-1">
+                  D <span className="mx-2"></span> CLIENT: {post.client}
+                </li>
+              </ul>
+            </div>
+            <div className="w-7/12 overflow-hidden flex justify-end relative">
+              <img
+                src={post.desktop}
+                className="w-10/12 rounded-xl shadow-md"
               />
-            </svg>
-          </a>
-          <div></div>
+              <a href={post.url}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 absolute bottom-5 right-5"
+                  style={{ color: post.textColor }}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
         </div>
       </main>
     </ProjectLayout>
