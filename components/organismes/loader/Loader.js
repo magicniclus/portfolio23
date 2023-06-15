aa2dca383a9b13c013f82e59b601ea71741df185
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-15 09:59:49 +0200

    Create Loader.js

---
+++ components/organismes/loader/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="h-screen fixed w-full bg-dark z-50 top-0 flex justify-center items-center text-clear">
+      Loading...
+    </div>
+  );
+};
+
+export default Loader;
