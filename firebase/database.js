3cec8716cf716bd4a74f1bd532b0a528f1cf9cc3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-22 10:04:54 +0200

    Update database.js

--- firebase/database.js
+++ firebase/database.js
@@ -2,21 +2,26 @@
 // import { getDatabase, ref, set } from "firebase/database";
 
 //Get user question from contact form with firebase realtime database
-// export function writeUserData(userId, name, surname, email, phone, message) {
-//   return new Promise((resolve, reject) => {
-//     const db = getDatabase();
-//     set(ref(db, "contacts/" + userId), {
-//       name: name,
-//       surname: surname,
-//       email: email,
-//       phone: phone,
-//       message: message,
-//     })
-//       .then(() => {
-//         resolve("success");
-//       })
-//       .catch((error) => {
-//         reject(error);
-//       });
-//   });
-// }
+import { database } from "./firebase.config";
+import { set, ref } from "firebase/database";
+
+export function writeUserData(name, company, phone, email, message, userId) {
+  return new Promise((resolve, reject) => {
+    set(ref(database, "contacts/" + userId), {
+      name,
+      company,
+      phone,
+      email,
+      message,
+      userId,
+      date: new Date().toLocaleString(),
+    })
+      .then(() => {
+        resolve("success");
+        new Date().toLocaleString();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
