45ae91d380f0ab395dce0ff26cd04bbfbf273510
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-24 19:03:02 +0200

    Update Loader.js

--- components/organismes/loader/Loader.js
+++ components/organismes/loader/Loader.js
@@ -22,7 +22,7 @@ const Loader = () => {
   }, []);
 
   return (
-    <div className="h-screen fixed w-full bg-dark z-50 top-0 flex justify-center items-center text-clear text-xl font-bold">
+    <div className="h-screen fixed w-full mx-auto max-w-[2000px] bg-dark z-50 top-0 flex justify-center items-center text-clear text-xl font-bold">
       {["L", "o", "a", "d", "i", "n", "g", ".", ".", "."].map((letter, i) => (
         <span className="letter opacity-0" key={i} ref={lettersRef[i]}>
           {letter}
